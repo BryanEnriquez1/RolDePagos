7ec898fc750b7171c877f8fc5d3a908289dbe911
Bryan Enriquez <bryan.enriquez487@ist17dejulio.edu.ec>
2023-12-10 15:39:26 -0500

    CREACION Y MODIFICACION DEL PAQUETE BUNDLE Y LA INTERFAZ DE LA ACTIVITY

--- app/src/main/java/com/baec/roldepagos/MainActivity.java
+++ app/src/main/java/com/baec/roldepagos/MainActivity.java
@@ -2,7 +2,15 @@ package com.baec.roldepagos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText nombresCom = findViewById(R.id.edNombre);
+        EditText anios = findViewById(R.id.edAnios);
+        EditText hijos = findViewById(R.id.edHijos);
+        EditText horas = findViewById(R.id.edHoras);
+        Button btncalcular = findViewById(R.id.btnCalculaSaldo);
+
+        Spinner cargos = (Spinner) findViewById(R.id.cbxCargos);
+        String [] opCargos = {
+                "GERENTE",
+                "EMPLEADO",
+                "CAJERO",
+                "ATENCION AL CLIENTE"
+        };
+        ArrayAdapter <String> CargosEmpresa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opCargos);
+        cargos.setAdapter(CargosEmpresa);
+
+        btncalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MuestraRol.class);
+                Bundle datos = new Bundle();
+                String nombreCompleto = nombresCom.getText().toString();
+                Integer anio = Integer.parseInt(anios.getText().toString());
+                Integer nHijos = Integer.parseInt(hijos.getText().toString());
+                Integer nHoras = Integer.parseInt(horas.getText().toString());
+                String cargo = cargos.getSelectedItem().toString();
+                datos.putString("nombreC",nombreCompleto);
+                datos.putInt("anios",anio);
+                datos.putInt("hijos",nHijos);
+                datos.putInt("horas",nHoras);
+                datos.putString("cargo",cargo);
+                intent.putExtras(datos);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/baec/roldepagos/MuestraRol.java
@@ -0,0 +1,14 @@
+package com.baec.roldepagos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MuestraRol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muestra_rol);
+    }
+}
